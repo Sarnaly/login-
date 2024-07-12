643a46aebd163b6830c159bc968c94bca0e4ba05
Sarnaly <128598668+Sarnaly@users.noreply.github.com>
2024-07-12 09:11:42 -0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<!-- Created By CodingNepal -->
+<html lang="en" dir="ltr">
+   <head>
+      <meta charset="utf-8">
+      <title>Popup Login Form Design | CodingNepal</title>
+      <link rel="stylesheet" href="css/style.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+   </head>
+   <body>
+    
+         
+         <div class="container">
+           
+            <div class="text">
+               Login Form
+            </div>
+            <form action="#" >
+               <div class="data">
+                  <label>Email or Phone</label>
+                  <input type="text" placeholder="Email or Phone" name="Email or Phone"required>
+               </div>
+               <div class="data">
+                  <label>Password</label>
+                  <input type="password" placeholder="Password" name="Password" required>
+               </div>
+               <div class="forgot-pass">
+                  <a href="#">Forgot Password?</a>
+               </div>
+               <div class="btn">
+                  <div class="inner"></div>
+                  <button type="submit">login</button>
+               </div>
+               <div class="signup-link">
+                  Not a member? <a href="#">Signup now</a>
+               </div>
+            </form>
+         </div>
+
+   </body>
+</html>
